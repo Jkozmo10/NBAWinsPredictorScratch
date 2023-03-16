28e889b746061a6a456deb6c90c94c24cac1fda7
LiamQuach <lquach03@calpoly.edu>
2023-03-15 20:12:57 -0700

    first change

--- Final_Project/src/main.java
+++ Final_Project/src/main.java
@@ -4,9 +4,8 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class main {
-    private static HashMap<Integer, HashMap<String, team>> data = new HashMap<>();
+    private static HashMap<Integer, team> data = new HashMap<>();
     private static HashMap<String, ArrayList<Double>>  toCalculate = new HashMap<>();
-
     public static void main(String[] args) {
         process("data.csv");
     }
@@ -17,6 +16,20 @@ public class main {
 
             // Skip the first line (header row)
             scanner.nextLine();
+            // Create key for the toCalculate
+            toCalculate.put("wins", new ArrayList<>());
+            toCalculate.put("losses", new ArrayList<>());
+            toCalculate.put("avgFgPercent", new ArrayList<>());
+            toCalculate.put("avgX3pPercent", new ArrayList<>());
+            toCalculate.put("avgFtPercent", new ArrayList<>());
+            toCalculate.put("avgTrbPerGame", new ArrayList<>());
+            toCalculate.put("avgAstPerGame", new ArrayList<>());
+            toCalculate.put("avgStlPerGame", new ArrayList<>());
+            toCalculate.put("avgBlkPerGame", new ArrayList<>());
+            toCalculate.put("avgTovPerGame", new ArrayList<>());
+            toCalculate.put("avgPtsPerGame", new ArrayList<>());
+
+            Integer counter = 0;
 
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
@@ -27,28 +40,37 @@ public class main {
                 String abbreviation = values[2].replace("\"", "");
                 Integer playoffs = values[3].equals("TRUE") ? 1 : 0;
                 int wins = Integer.parseInt(values[4]);
+                toCalculate.get("wins").add((double) wins);
                 int losses = Integer.parseInt(values[5]);
+                toCalculate.get("losses").add((double) losses);
                 double avgFgPercent = Double.parseDouble(values[6]);
+                toCalculate.get("avgFgPercent").add(avgFgPercent);
                 double avgX3pPercent = Double.parseDouble(values[7]);
+                toCalculate.get("avgX3pPercent").add(avgX3pPercent);
                 double avgFtPercent = Double.parseDouble(values[8]);
+                toCalculate.get("avgFtPercent").add(avgFtPercent);
                 double avgTrbPerGame = Double.parseDouble(values[9]);
+                toCalculate.get("avgTrbPerGame").add(avgTrbPerGame);
                 double avgAstPerGame = Double.parseDouble(values[10]);
+                toCalculate.get("avgAstPerGame").add(avgAstPerGame);
                 double avgStlPerGame = Double.parseDouble(values[11]);
+                toCalculate.get("avgStlPerGame").add(avgStlPerGame);
                 double avgBlkPerGame = Double.parseDouble(values[12]);
+                toCalculate.get("avgBlkPerGame").add(avgBlkPerGame);
                 double avgTovPerGame = Double.parseDouble(values[13]);
+                toCalculate.get("avgTovPerGame").add(avgTovPerGame);
                 double avgPtsPerGame = Double.parseDouble(values[14]);
+                toCalculate.get("avgPtsPerGame").add(avgPtsPerGame);
 
                 team newTeam = new team(season, abbreviation, playoffs, wins, losses, avgFgPercent, avgX3pPercent, avgFtPercent, avgTrbPerGame, avgAstPerGame, avgStlPerGame, avgBlkPerGame, avgTovPerGame, avgPtsPerGame);
                 System.out.println(newTeam);
                 // If the season key doesn't exist, add it
-                if (!data.containsKey(season)) {
-                    data.put(season, new HashMap<String, team>());
-                }
 
+                data.put(counter, newTeam);
+                counter++;
                 // Add the team to the season
-                data.get(season).put(abbreviation, newTeam);
-            }
 
+            }
             scanner.close();
         } catch (FileNotFoundException e) {
             System.out.println("File not found: " + filename);
